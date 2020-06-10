20dc8bb64abfed9ac4c8123f64bac806a6b0b28c
khotphet <profess538@gmail.com>
2020-06-10 15:59:11 +0700

    2 files modified

--- App.js
+++ App.js
@@ -9,9 +9,15 @@ import FoodItem from './components/FoodItem'
 
 
 class App extends Component{
+    this.state={
+    items:[{id:1, "name": "กระเพราไก่"},{id:2,name:"ผัดคะน้า"}],
+    
+  }
+
   render(){
   return(
     <div className="container">
+    <h2>Food name Application</h2>
     <div className="row">
     
     <FoodInput/>
--- components/FoodInput.js
+++ components/FoodInput.js
@@ -1,12 +1,21 @@
 import React, {Component} from "react";
 
 export default class FoodInput extends Component{
+
+
+
+
+
+
   render(){
     return(
       <div className="card card-body my-3">
           <form>
+          <div className="form-group">
+          <label>ป้อนรายการอาหาร</label>
             <input type="text" class="form-control" value=""/>
-            
+            </div>
+            <input type="submit" class="btn btn-primary" value="บันทึกรายการ" />
           
           
           </form>
