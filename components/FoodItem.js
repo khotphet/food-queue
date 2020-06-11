680c38f6425b04429a1775f2c5b80242e6f694ef
khotphet <profess538@gmail.com>
2020-06-11 17:42:44 +0700

    1 file modified

--- components/FoodItem.js
+++ components/FoodItem.js
@@ -2,14 +2,15 @@ import React, {Component} from "react";
 
 export default class FoodItem extends Component{
   render(){
-       const {name,handleDelete,handleEdit}= this.props
+       const {name, handleEdit, handleDelete}= this.props
     return(
         <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
         
-        <h6>{name}</h6>        
+        <h6>{name}</h6>       
         <div>
-        <span onClick={handleEdit}><i className="fas fa-edit"></i></span>
-        <span onClick={handleDelete}><i className="far fa-trash-alt"></i></span>
+
+        <span onClick={this.handleEdit}><i className="fas fa-edit"></i></span>
+        <span onClick={this.handleDelete}><i className="far fa-trash-alt"></i></span>
         </div>
         </li>
     )
