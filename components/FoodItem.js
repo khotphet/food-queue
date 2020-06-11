090bf5f68278faa7039c98c6f7b598f2db3a376d
khotphet <profess538@gmail.com>
2020-06-11 16:43:48 +0700

    2 files modified

--- components/FoodItem.js
+++ components/FoodItem.js
@@ -7,6 +7,10 @@ export default class FoodItem extends Component{
         <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
         
         <h6>{name}</h6>        
+        <div>
+        <span onClick={handleEdit}><i className="fas fa-edit"></i></span>
+        <span onClick={handleDelete}><i class="far fa-trash-alt"></i></span>
+        </div>
         </li>
     )
   }
